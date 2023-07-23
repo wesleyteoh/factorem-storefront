3867e838f2c5c0d25a70a029bd6e51d57d4116ed
genotabby <teohwesley@gmail.com>
2023-07-23 12:25:24 +0800

    Added discontinued label

--- src/Components/PastPurchases/PastPurchases.jsx
+++ src/Components/PastPurchases/PastPurchases.jsx
@@ -7,6 +7,7 @@ export default function PastPurchases({ purchases }) {
         {purchases.products.map((item) => (
           <div key={item.product_id}>
             <fieldset>
+              {/* <div>{JSON.stringify(item.product_active)}</div> */}
               <img width="15%" src={item.image_link} alt="product_image" />
               <div>Product name: {item.product_name}</div>
               <div>Material: {item.material_category_name}</div>
@@ -14,6 +15,7 @@ export default function PastPurchases({ purchases }) {
               <div>Order quantity: {item.order_quantity}</div>
               <div>Lead time: {item.leadtime} days</div>
               <div>Status: {item.shipping_type}</div>
+              {item.product_active ? <></> : <div>DISCONTINUED</div>}
               <div>
                 Total Price: $
                 {(item.unit_price * item.order_quantity).toFixed(2)}
