1b24cf635b5694252cfe45b4cebdb85e6c821e97
genotabby <teohwesley@gmail.com>
2023-07-25 20:09:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const path = require("path");
-// const favicon = require("serve-favicon");
+const favicon = require("serve-favicon");
 const logger = require("morgan");
 
 require("dotenv").config();
 // const pool = require("./config/database");
-// require("./config/database");
 const app = express();
+app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(logger("dev"));
 app.use(express.json());
